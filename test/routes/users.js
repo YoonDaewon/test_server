51c6f432f3fc4c3f42274773b9730cf5e56a815b
YoonDaewon <ydwinha@naver.com>
2017-07-18 18:10:31 +0900

    커밋테스트1

--- test/routes/users.js
+++ test/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('connection test2222');
+  res.send('Connection ok');
 });
 
 module.exports = router;
